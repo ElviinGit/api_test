1c269ccba80cf6e128d4dd55a8c5b8df549e2dd8
Elvin <emelikov78@gmail.com>
2026-07-18 17:13:37 +0400

    kwarg function is modified with some dofault data

--- kwarg.py
+++ kwarg.py
@@ -1,7 +1,33 @@
-def my_kwarg(*args, **kwargs):
-    return f"my arguments are: {args}, my keyword arguments are: {kwargs}"
+# def addition(*args):
+#     return sum(args)
+
+# result = addition(5, 3, 8, 2)
+
+# print("The result of addition is:", result)
+
+# def greet_user(**kwargs):
+#     name = kwargs.get("name", "anonymous")
+#     place = kwargs.get("place", "unknown")
+#     return f"Hello, {name} from {place}!"
+
+# greeting_alice = greet_user(name="Alice")
+# greeting_default = greet_user() 
+
+# print(greeting_alice)  # Output: Hello, Alice from Wonderland!
 
 
-simple = my_kwarg("first", "second", env="qa", debug=True)  
+# def print_details(**kwargs):
+#     age = kwargs.get("age") 
+#     print(f"Age is {age}")
 
-print(simple)
+# my_details = print_details()  # Output: Age is 30
+# # print(my_details)  # Output: Age is 30
+
+def greet(**kwargs):
+    name = kwargs.get("name", "Guest")
+    age = kwargs.get("age")
+    return f"Hello {name}, age {age}"
+
+print(greet(name="Elvin"))
+print(greet())  # Hello Guest, age unknown
+    
